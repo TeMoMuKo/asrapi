9cc05d19efa02a529262ead1de22496fac3b827a
Michał Zając <michal.zajac@gmail.com>
2016-01-05 20:19:40 +0100

    Validations and basic model logic

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,7 @@
 class Location < ApplicationRecord
+  validates :latitude, numericality: true, presence: true
+  validates :longitude, numericality: true, presence: true
+
   belongs_to :team
+  validates :team, presence: true
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,2 +1,13 @@
 class Team < ApplicationRecord
+  validates :name, presence: true
+
+  has_many :locations, dependent: :destroy
+
+  def last_location
+    locations.order('created_at DESC').first(1)
+  end
+
+  def to_s
+    name
+  end
 end
