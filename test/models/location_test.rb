f2f9ceb5b9427f6933938da10b9f4962bfa70172
Michał Zając <michal.zajac@gmail.com>
2016-01-05 20:18:29 +0100

    Add tests.

--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -1,7 +1,18 @@
 require 'test_helper'
 
 class LocationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should not save a Location without latitude' do
+    location = Location.new(longitude: 21.37, team: teams(:team1))
+    assert_not location.save, "Saved a Location without latitude"
+  end
+
+  test 'should not save a Location without longitude' do
+    location = Location.new(latitude: 21.37, team: teams(:team1))
+    assert_not location.save, "Saved a Location without longitude"
+  end
+
+  test 'should not save a Location without a Team' do
+    location = Location.new(latitude: 21.37, longitude: 37.21)
+    assert_not location.save, "Saved a Location without a Team"
+  end
 end
