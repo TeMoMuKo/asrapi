1d7fc86268af43ec3a9e7cb2398f6499c3f74e26
Michał Zając <michal.zajac@gmail.com>
2016-01-21 23:05:06 +0100

    As of I now I can into decimal

--- db/migrate/20160102175020_create_locations.rb
+++ db/migrate/20160102175020_create_locations.rb
@@ -1,8 +1,8 @@
 class CreateLocations < ActiveRecord::Migration[5.0]
   def change
     create_table :locations do |t|
-      t.decimal :latitude, scale: 10, precision: 6
-      t.decimal :longitude, scale: 10, precision: 6
+      t.decimal :latitude, precision: 10, scale:6
+      t.decimal :longitude, precision: 10, scale: 6
       t.string :message, null: false, default: ""
       t.references :team, index: true, foreign_key: true
 
