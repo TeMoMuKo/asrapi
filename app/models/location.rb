47f62498d8d1f228726c4d01566dd44686f968a6
Michał Zając <michal.zajac@gmail.com>
2016-01-25 18:56:27 +0100

    Nulls, pls go and stay go

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,7 @@
 class Location < ApplicationRecord
   validates :latitude, numericality: true, presence: true, latitude: true
   validates :longitude, numericality: true, presence: true, longitude: true
-  validates :message, length: { maximum: 160 }
+  validates :message, length: { maximum: 160 }, exclusion: { in: [nil] }
 
   belongs_to :team
   validates :team, presence: true
