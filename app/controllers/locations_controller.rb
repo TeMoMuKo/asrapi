b2084c18b8371778531f2eba053bc61df728776d
Michał Zając <michal.zajac@gmail.com>
2016-01-25 12:03:07 +0100

    Initial documentation, needs examples

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,15 +2,34 @@ class LocationsController < ApplicationController
   before_action :set_team, only: [:index, :create]
   before_action :set_location, only: [:show]
 
+  ##
+  # Returns a list of Locations for a given Team
+  #
+  # GET /team/:team_id/locations
+  #
+  # params:
+  #     team_id - id of Team to fetch locations for
   def index
     @locations = @team.locations
   end
 
+  ##
+  # Returns a given location
+  #
   # GET /locations/1
+  #
+  # params:
+  #     location_id - id of Location to fetch
   def show
   end
 
+  ##
+  # Create a new location for a given team
+  #
   # POST /teams/:team_id/locations
+  #
+  # params:
+  #     team_id - id of Team to create a location for
   def create
     @location = @team.locations.new(location_params)
     if @location.save
--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,12 +1,21 @@
 class TeamsController < ApplicationController
   before_action :set_team, only: [:show]
 
+  ##
+  # Returns a list of all Teams with their last known location
+  #
   # GET /teams
   def index
     @teams = Team.all
   end
 
+  ##
+  # Returns a given Team
+  #
   # GET /teams/:team_id
+  #
+  # params:
+  #     team_id - id of Team to fetch
   def show
   end
 
