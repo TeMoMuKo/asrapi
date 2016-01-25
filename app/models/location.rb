ab0f83365158a6ff8d0a36a76a374cfda0401e4d
Michał Zając <michal.zajac@gmail.com>
2016-01-25 18:29:45 +0100

    Location message can now have only 160 characters.

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,7 @@
 class Location < ApplicationRecord
   validates :latitude, numericality: true, presence: true
   validates :longitude, numericality: true, presence: true
+  validates :message, length: { maximum: 160 }
 
   belongs_to :team
   validates :team, presence: true
