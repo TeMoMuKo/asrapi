167e036fec5929393d46616f2db06ba5877d96a2
Michał Zając <michal.zajac@gmail.com>
2016-02-14 19:23:48 +0100

    Sort Locations by default

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,5 @@
 class Location < ApplicationRecord
+  default_scope { order('created_at DESC') }
   validates :latitude, numericality: true, presence: true, latitude: true
   validates :longitude, numericality: true, presence: true, longitude: true
   validates :message, length: { in: 0..160 }
