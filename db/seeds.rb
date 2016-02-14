27adc2014d2e6d780df7058be68762dc690b87fe
Michał Zając <michal.zajac@gmail.com>
2016-02-14 12:35:02 +0100

    New seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,21 @@
-user = User.new(first_name: "Sebastian", last_name: "Aralkiewicz", email: "seba@test.pl", password: "qweasdzxc")
+team = Team.create(name: "Team 1")
+user = User.new(first_name: "Sebastian", last_name: "Aralkiewicz", email: "seba1@test.pl", password: "qweasdzxc")
 user.skip_confirmation!
+user.team = team
 user.save!
 
-team = Team.create(name: "Team 1")
+user = User.new(first_name: "Sebastian", last_name: "Aralkiewicz", email: "seba2@test.pl", password: "qweasdzxc")
+user.skip_confirmation!
 user.team = team
-user.save
+user.save!
 
-team.locations.create(latitude: 21.37, longitude: 37.21)
-team.locations.create(latitude: 37.21, longitude: 21.37)
+team = Team.create(name: "Team 2")
+user = User.new(first_name: "Sebastian", last_name: "Aralkiewicz", email: "seba3@test.pl", password: "qweasdzxc")
+user.skip_confirmation!
+user.team = team
+user.save!
+
+user = User.new(first_name: "Sebastian", last_name: "Aralkiewicz", email: "seba4@test.pl", password: "qweasdzxc")
+user.skip_confirmation!
+user.team = team
+user.save!
