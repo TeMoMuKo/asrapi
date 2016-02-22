b02952df3786d2ad47ca39d6cd7a8f509820408a
Michał Zając <michal.zajac@gmail.com>
2016-02-22 18:49:56 +0100

    Reverse geocode on save

--- app/models/location.rb
+++ app/models/location.rb
@@ -5,4 +5,12 @@ class Location < ApplicationRecord
   validates :message, length: { in: 0..160 }
   belongs_to :team
   validates :team, presence: true
+  reverse_geocoded_by :latitude, :longitude do |obj, results|
+    if geo = results.first
+      obj.address = geo.address
+      obj.country = geo.country
+      obj.country_code = geo.country_code
+    end
+  end
+  after_validation :reverse_geocode, if: ->(obj){ (obj.latitude.present? && obj.longitude.present?) and (obj.latitude_changed? && obj.longitude_changed?) }
 end
