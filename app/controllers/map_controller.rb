d38910fe76e35551393c3942705faf05961f840d
Michał Zając <michal.zajac@gmail.com>
2016-04-19 11:35:27 +0200

    Handle GET parameteres in map

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,5 +1,6 @@
 class MapController < ApplicationController
   def index
+    @team_no = params[:team].delete("^0-9") if params[:team]
     @teams = Team.all
     @markers = Hash[Team.all.map { |team| [team.number, team.markers] }].to_json
   end
