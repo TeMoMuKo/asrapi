f25c21862220b84c16355c888159de72958aa085
Michał Zając <michal.zajac@gmail.com>
2016-04-19 17:27:31 +0200

    Change title depending on link

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,6 +1,9 @@
 class MapController < ApplicationController
   def index
-    @team_no = params[:team].delete("^0-9") if params[:team]
+    if params[:team]
+      @team_no = params[:team].delete("^0-9")
+      @title = "Mapa lokalizacji zespołu #{@team_no}" unless @team_no.empty?
+    end
     @teams = Team.all
     @markers = Hash[Team.all.map { |team| [team.number, team.markers] }].to_json
   end
