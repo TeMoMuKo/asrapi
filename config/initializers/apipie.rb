c3773c40b37d0b34dc42cf96296d8fdf66ad0576
Michał Zając <michal.zajac@gmail.com>
2017-02-24 01:32:24 +0100

    We V2 now

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -4,4 +4,5 @@ Apipie.configure do |config|
   config.doc_base_url            = "/api/docs"
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/api/**/*.rb"
   config.validate                = false
+  config.default_version         = "2.0"
 end
